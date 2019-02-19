55049b22b6c026a54f28ca9443fb072223882841
Spencer Harmon <spencer.harmon@gmail.com>
2019-02-19 14:36:54 -0700

    final push

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,27 +21,28 @@ class NotesController < ApplicationController
     end
   end
 
-    private
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  private
     def notes_param
       params.require(:note).permit(:title, :color, :body)
     end
 
-    def edit
-      @note = Note.find(params[:id])
-    end
-
-    def update
-      @note = Note.find(params[:id])
-      
-      if @note.update(note_params)
-        redirect_to notes_path
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      Note.find(params[:id]).destroy
-      redirect_to notes_path
-    end
 end
